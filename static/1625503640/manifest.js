222e8b35109ae889c3c2b2e1b15596debc8d9073
tasticad <tasticad@users.noreply.github.com>
2021-07-05 16:47:25 +0000

    deploy: f52a96211d7b844a1363b838092c10934c5b4122

---
+++ static/1625503640/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fsignup","\u002F","\u002Fateliers-stages","\u002Factivites","\u002Finstrumentarium","\u002Fcreations-tous-terrains","\u002Fecouter-voir","\u002Fpedagogie-de-la-creation","\u002Factualites","\u002Fpresse","\u002Fpresentation"]})
